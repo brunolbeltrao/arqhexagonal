3574f5eee109af7dc8e7908a050d659434995d54
Bruno Leite Beltrao <bleiteb@indracompany.com>
2022-06-16 10:32:36 -0300

    Dominio

--- src/main/java/conta/sistema/doinio/modelo/Conta.java
+++ src/main/java/conta/sistema/doinio/modelo/Conta.java
@@ -2,16 +2,53 @@ package conta.sistema.doinio.modelo;
 
 import java.math.BigDecimal;
 
+import static conta.sistema.doinio.modelo.Erro.obrigatorio;
+import static conta.sistema.doinio.modelo.Erro.saldoInsuficiente;
+import static java.util.Objects.isNull;
+
+
+
 public class Conta {
     private Integer numero;
     private BigDecimal saldo;
     private String correntista;
 
     public Conta() {
-        this.numero = 0;
-        this.saldo = BigDecimal.ZERO;
-        this.correntista = "não informado";
+        numero = 0;
+        saldo = BigDecimal.ZERO;
+        correntista = "não informado";
     }
+
+    public Conta(Integer numero, BigDecimal saldo, String correntista) {
+        this.numero = numero;
+        this.saldo = saldo;
+        this.correntista = correntista;
+    }
+
+    public void creditar(BigDecimal credito) {
+        if (isNull(credito)) {
+            obrigatorio("Valor crédito");
+        }
+        if (credito.compareTo(BigDecimal.ZERO) <= 0) {
+            obrigatorio("Valor crédito");
+        }
+        saldo = saldo.add(credito);
+    }
+
+    public void debitar(BigDecimal debito) {
+        if (isNull(debito)) {
+            obrigatorio("Valor débito");
+        }
+        if (debito.compareTo(BigDecimal.ZERO) <= 0) {
+            obrigatorio("Valor débito");
+        }
+        if (debito.compareTo(saldo) > 0) {
+            saldoInsuficiente();
+        }
+        saldo = saldo.subtract(debito);
+    }
+
+    // gets e sets
 
     public Integer getNumero() {
         return numero;
@@ -34,12 +71,6 @@ public class Conta {
     }
 
     public void setCorrentista(String correntista) {
-        this.correntista = correntista;
-    }
-
-    public Conta(Integer numero, BigDecimal saldo, String correntista) {
-        this.numero = numero;
-        this.saldo = saldo;
         this.correntista = correntista;
     }
 
--- src/main/java/conta/sistema/doinio/modelo/Erro.java
+++ src/main/java/conta/sistema/doinio/modelo/Erro.java
@@ -1,16 +1,21 @@
 package conta.sistema.doinio.modelo;
 
 public class Erro {
-    public static void obrigatorio(String nome){
-        throw new NegocioException(nome + "é obrigatório.");
+    // eros genéricos
+    public static void obrigatorio(String nome) {
+        throw new NegocioException(nome + " é obrigatório.");
     }
-    public static void inexistente(String nome){
-        throw new NegocioException(nome + "é inexistente.");
+
+    public static void inexistente(String nome) {
+        throw new NegocioException(nome + " é inexistente.");
     }
-    public static void saldoInsuficiente(String nome){
+
+    // eros especificos
+    public static void saldoInsuficiente() {
         throw new NegocioException("Saldo insuficiente.");
     }
-    public static void mesmaConta(){
+
+    public static void mesmaConta() {
         throw new NegocioException("Conta débito e crédito devem ser diferentes.");
     }
 
